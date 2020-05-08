9ba5dcc4fe145d790ca7692dc1ad9aa8f73d775d
EC2 Default User <ec2-user@ip-172-31-25-85.ec2.internal>
2020-05-08 17:42:15 +0000

    changed dependency for null file

--- AWS/jenkins_project/null_resource.tf
+++ AWS/jenkins_project/null_resource.tf
@@ -1,10 +1,29 @@
 resource "null_resource" "jenkins_passwd" {
-  depends_on = ["aws_instance.jenkins_server"]
+  depends_on = ["aws_route_table.private_rt"]
 
   triggers = {
     always_run = "${timestamp()}"
   }
 
+#   provisioner "remote-exec" {
+#     connection {
+#       type        = "ssh"
+#       user        = "ec2-user"
+#       private_key = "${file("~/.ssh/id_rsa")}"
+#       host        = "${aws_instance.jenkins_server.public_ip}"
+#     }
+
+#     inline = [
+#       "sudo yum install java-1.8.0-openjdk-devel -y",
+#       "curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo",
+#       "sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key",
+#       "sudo yum install jenkins -y",
+#       "sudo systemctl start jenkins",
+#       "sudo systemctl enable jenkins",
+#       "echo -e $(tput setaf 1 )'Jenkins initialAdminPassword is: '$(tput sgr0) $(tput setaf 2)`sudo cat /var/lib/jenkins/secrets/initialAdminPassword`$(tput sgr0)",
+#     ]
+#   }
+# }
   provisioner "remote-exec" {
     connection {
       type        = "ssh"
@@ -12,15 +31,8 @@ resource "null_resource" "jenkins_passwd" {
       private_key = "${file("~/.ssh/id_rsa")}"
       host        = "${aws_instance.jenkins_server.public_ip}"
     }
-
     inline = [
-      "sudo yum install java-1.8.0-openjdk-devel -y",
-      "curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo",
-      "sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key",
-      "sudo yum install jenkins -y",
-      "sudo systemctl start jenkins",
-      "sudo systemctl enable jenkins",
-      "echo -e $(tput setaf 1 )'Jenkins initialAdminPassword is: '$(tput sgr0) $(tput setaf 2)`sudo cat /var/lib/jenkins/secrets/initialAdminPassword`$(tput sgr0)",
+      "echo -e $(tput setaf 1 )'Jenkins Passwd is: '$(tput sgr0) $(tput setaf 2)`sudo cat /var/lib/jenkins/secrets/initialAdminPassword`$(tput sgr0)",
     ]
   }
 }
